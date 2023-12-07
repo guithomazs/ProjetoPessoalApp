f3c8976181a4ad5b595e0439fda2a56492a41bfb
unknown <guithomazs@gmail.com>
2023-12-06 20:31:11 -0500

    Edições no modelo do banco

--- back-end/sql/cfg.sql
+++ back-end/sql/cfg.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS `participante`;
 CREATE TABLE `participante` (
 	`id` bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
+  `senha` varchar(255) DEFAULT NULL,
   `apelido` varchar(255)
 );
 
@@ -25,12 +26,25 @@ CREATE TABLE `compra` (
 
 DROP TABLE IF EXISTS `compra_participante`;
 CREATE TABLE `compra_participante` (
-	`id` bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `quantidade_a_pagar` DECIMAL(6,2) NOT NULL,
-    `compra_id` bigint NOT NULL,
-    `participante_id` bigint NOT NULL,
-    KEY `FK_compra` (`compra_id`),
-	KEY `FK_participante` (`participante_id`),
-	CONSTRAINT `FK_compra` FOREIGN KEY (`compra_id`) REFERENCES `compra` (`id`),
+  `quantidade_a_pagar` DECIMAL(6,2) NOT NULL,
+  `compra_id` bigint NOT NULL,
+  `participante_id` bigint NOT NULL,
+  PRIMARY KEY(`compra_id`,`participante_id`),
+  KEY `FK_compra` (`compra_id`),
+  KEY `FK_participante` (`participante_id`),
+  CONSTRAINT `FK_compra` FOREIGN KEY (`compra_id`) REFERENCES `compra` (`id`),
 	CONSTRAINT `FK_participante` FOREIGN KEY (`participante_id`) REFERENCES `participante` (`id`)
 );
+
+DROP TABLE IF EXISTS `divida_participante`;
+CREATE TABLE `divida_participante` (
+  `participante1_id` bigint NOT NULL,
+  `participante2_id` bigint NOT NULL,
+  `divida_p1_para_p2` decimal(10,2) NOT NULL,
+  `divida_p2_para_p1` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`participante1_id`, `participante2_id`),
+  KEY `FK_participante1` (`participante1_id`),
+  KEY `FK_participante2` (`participante2_id`),
+	CONSTRAINT `FK_participante1` FOREIGN KEY (`participante1_id`) REFERENCES `participante` (`id`),
+	CONSTRAINT `FK_participante2` FOREIGN KEY (`participante2_id`) REFERENCES `participante` (`id`)
+)
--- back-end/sql/cfgDados.sql
+++ back-end/sql/cfgDados.sql
@@ -13,18 +13,30 @@ UNLOCK TABLES;
 
 LOCK TABLES `compra` WRITE;
 INSERT INTO `compra` VALUES
-    (1, 7, '12.00', '2023-04-20', '12:00:00', 'Marlene', 'Coca-cola'),
-    (2, 7, '5.34', '2023-04-20', '14:00:00', 'Marlene', 'Coca-cola'),
-    (3, 7, '7.30', '2023-04-20', '14:30:00', 'Marlene', 'Coca-cola'),
-    (4, 7, '11.69', '2023-04-20', '15:00:00', 'Marlene', 'Coca-cola'),
-    (5, 7, '15.00', '2023-04-20', '15:30:00', 'Engenharias', 'Coca-cola');
+    (DEFAULT, 4, '15.50', '2023-04-20', '14:00:00', 'Marlene', 'Coca-cola'),
+    (DEFAULT, 1, '7.50', '2023-04-20', '14:30:00', 'Marlene', 'Coca-cola'),
+    (DEFAULT, 6, '12', '2023-04-20', '15:00:00', 'Marlene', 'Coca-cola'),
+    (DEFAULT, 7, '15.00', '2023-04-20', '15:30:00', 'Engenharias', 'Coca-cola');
 UNLOCK TABLES;
 
+-- valor | compra | participante
 LOCK TABLES `compra_participante` WRITE;
 INSERT INTO `compra_participante` VALUES
-	(DEFAULT, '5', 5, 1),
-    (DEFAULT, '5', 5, 2),
-    (DEFAULT, '5', 5, 3),
-    (DEFAULT, '5', 5, 4),
-    (DEFAULT, '5', 5, 5);
+	(DEFAULT, '3', 1, 4),
+	(DEFAULT, '6.50', 1, 3),
+	(DEFAULT, '3', 1, 1),
+	(DEFAULT, '3', 1, 6),
+	(DEFAULT, '3', 2, 1),
+	(DEFAULT, '4.50', 2, 3),
+	(DEFAULT, '4', 3, 6),
+	(DEFAULT, '4', 3, 1),
+	(DEFAULT, '4', 3, 3),
+	(DEFAULT, '5', 4, 7),
+	(DEFAULT, '5', 4, 1),
+	(DEFAULT, '5', 4, 3),
+UNLOCK TABLES;
+
+LOCK TABLES `divida_participante` WRITE;
+INSERT INTO `divida_participante` VALUES
+	
 UNLOCK TABLES;
